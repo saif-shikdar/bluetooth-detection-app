b6c897ddd6fb0b8f573175b9235e196f1aa012f4
Saif Shikdar <43826661+saif-shikdar@users.noreply.github.com>
2024-02-06 17:08:30 +0000

    Initial commit

---
+++ bluetooth-detection/bluetooth-detection/bluetooth_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetooth_detectionApp.swift
+//  bluetooth-detection
+//
+//  Created by Saif Shikdar on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct bluetooth_detectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
